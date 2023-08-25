aef4b91a0a9efa70669960c69a2dc074ca3b8ff9
Almog Gabay <theninesky@gmail.com>
2023-08-25 18:35:33 +0300

    Set up the server

--- index.js
+++ index.js
@@ -1,42 +1,62 @@
+import http from 'node:http'
 import puppeteer from 'puppeteer'
 
 import resourcesToBlock from './utils/resourcesToBlock.js'
 import removeScriptTags from './utils/removeScriptTags.js'
 import removePreloads from './utils/removePreloads.js'
 
-const launch = async () => {
-  const browser = await puppeteer.launch({ headless: 'new' })
+const { PORT = 8000 } = process.env
+const PRERENDER_USER_AGENT = 'Prerender'
 
-  console.log('Started Headless Chrome')
+const browser = await puppeteer.launch({ headless: 'new' })
+
+console.log('Started Headless Chrome')
+
+const server = http.createServer(async (req, res) => {
+  const { url, width = 1440 } = Object.fromEntries(
+    req.url
+      .split('?')[1]
+      .split('&')
+      .map(param => param.split('='))
+  )
 
   const page = await browser.newPage()
 
-  await page.setViewport({ width: 1440, height: 768 })
-  await page.setRequestInterception(true)
+  try {
+    console.log(`Requesting ${url}`)
 
-  page.on('request', interceptedRequest => {
-    if (interceptedRequest.isInterceptResolutionHandled()) return
-    if (resourcesToBlock.some(resource => interceptedRequest.url().endsWith(resource))) interceptedRequest.abort()
-    else interceptedRequest.continue()
-  })
+    await page.setUserAgent(PRERENDER_USER_AGENT)
+    await page.setViewport({ width: Number(width), height: 768 })
+    await page.setRequestInterception(true)
 
-  await page.goto('https://client-side-rendering.pages.dev/web-vitals')
-  await page.deleteCookie(...(await page.cookies()))
-  await page.evaluate(() => {
-    localStorage.clear()
-    window.prerender = true
-  })
-  await page.waitForNetworkIdle({ idleTime: 200 })
+    page.on('request', interceptedRequest => {
+      if (interceptedRequest.isInterceptResolutionHandled()) return
+      if (resourcesToBlock.some(resource => interceptedRequest.url().endsWith(resource))) interceptedRequest.abort()
+      else interceptedRequest.continue()
+    })
 
-  let html = await page.evaluate(() => document.documentElement.outerHTML)
+    await page.goto(url)
+    await page.deleteCookie(...(await page.cookies()))
+    await page.evaluate(() => localStorage.clear())
+    await page.waitForNetworkIdle({ idleTime: 100 })
 
-  html = removeScriptTags(html)
-  html = removePreloads(html)
+    let html = await page.evaluate(() => document.documentElement.outerHTML)
 
-  console.log(html)
+    html = removeScriptTags(html)
+    html = removePreloads(html)
+
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+    res.end(html)
+
+    console.log(`Request sent for ${url}\n`)
+  } catch (err) {
+    console.error(err)
+
+    res.writeHead(404)
+    res.end()
+  }
 
   page.close()
-  browser.close()
-}
+})
 
-launch()
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}\n`))
