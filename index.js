a2d79c0743a43004067d47905b65fd483cfddf7d
Almog Gabay <theninesky@gmail.com>
2023-08-31 08:53:43 +0300

    Wait for navigation

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import removePreloads from './utils/removePreloads.js'
 const numCPUs = availableParallelism()
 
 const {
-  CPUS = Math.min(numCPUs - 1, 10),
+  CPUS = numCPUs - 1,
   PORT = 8000,
   RATE_LIMIT = 100,
   USER_AGENT = 'Prerender',
@@ -89,7 +89,7 @@ const renderPage = async websiteUrl => {
 
   console.log(`Requesting ${websiteUrl} (#${tabID})`)
 
-  await page.evaluate(url => window.navigateTo(url), websiteUrl)
+  await Promise.all([page.waitForNavigation(), page.evaluate(url => window.navigateTo(url), websiteUrl)])
 
   try {
     await page.waitForNetworkIdle({ idleTime: +WAIT_AFTER_LAST_REQUEST, timeout: +WAIT_AFTER_LAST_REQUEST_TIMEOUT })
@@ -105,3 +105,5 @@ const renderPage = async websiteUrl => {
 }
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}\n`))
+
+process.on('exit', () => browser.close())
