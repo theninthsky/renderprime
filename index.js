d1704025b39fdf60b3c177a105b0bc26dab0e5b5
Almog Gabay <theninesky@gmail.com>
2024-03-24 08:13:29 +0200

    Properly convert to a GCP function

--- index.js
+++ index.js
@@ -1,61 +1,38 @@
-import http from 'node:http'
-import url from 'node:url'
+import functions from '@google-cloud/functions-framework'
 import puppeteer from 'puppeteer'
+import url from 'node:url'
 
 import resourcesToBlock from './utils/resourcesToBlock.js'
 import removeScriptTags from './utils/removeScriptTags.js'
 import removePreloads from './utils/removePreloads.js'
 
-const {
-  PORT = 8000,
-  USER_AGENT = 'Prerender',
-  WAIT_AFTER_LAST_REQUEST = 200,
-  WAIT_AFTER_LAST_REQUEST_TIMEOUT = 5000
-} = process.env
+const { USER_AGENT = 'Prerender', WAIT_AFTER_LAST_REQUEST = 200, WAIT_AFTER_LAST_REQUEST_TIMEOUT = 5000 } = process.env
 
-const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox'] })
-const [page] = await browser.pages()
-
-await page.setUserAgent(USER_AGENT)
-await page.setViewport({ width: 1440, height: 768 })
-await page.setRequestInterception(true)
-
-page.on('request', interceptedRequest => {
-  if (interceptedRequest.isInterceptResolutionHandled()) return
-  if (resourcesToBlock.some(resource => interceptedRequest.url().endsWith(resource))) {
-    return interceptedRequest.abort()
-  }
-
-  interceptedRequest.continue()
-})
-
-const server = http.createServer(async (req, res) => {
+functions.http('render', async (req, res) => {
   const { query } = url.parse(req.url, true)
+  const websiteUrl = query.url
 
-  if (!query.url) {
+  if (!websiteUrl) {
     res.writeHead(400)
     return res.end()
   }
 
-  try {
-    let html = await renderPage(query.url)
+  const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox'] })
+  const [page] = await browser.pages()
 
-    html = removeScriptTags(html)
-    html = removePreloads(html)
+  await page.setUserAgent(USER_AGENT)
+  await page.setViewport({ width: 1440, height: 768 })
+  await page.setRequestInterception(true)
 
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
-    res.end(html)
+  page.on('request', interceptedRequest => {
+    if (interceptedRequest.isInterceptResolutionHandled()) return
+    if (resourcesToBlock.some(resource => interceptedRequest.url().endsWith(resource))) {
+      return interceptedRequest.abort()
+    }
 
-    console.log(`Request sent for ${query.url}`)
-  } catch (err) {
-    console.error(err)
+    interceptedRequest.continue()
+  })
 
-    res.writeHead(503)
-    res.end()
-  }
-})
-
-const renderPage = async websiteUrl => {
   console.log(`Requesting ${websiteUrl}`)
 
   let html
@@ -65,14 +42,14 @@ const renderPage = async websiteUrl => {
     await page.waitForNetworkIdle({ idleTime: +WAIT_AFTER_LAST_REQUEST, timeout: +WAIT_AFTER_LAST_REQUEST_TIMEOUT })
 
     html = await page.evaluate(() => document.documentElement.outerHTML)
+    html = removeScriptTags(html)
+    html = removePreloads(html)
+
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+    res.end(html)
   } catch (err) {
     console.error(err.message)
-
-    html = await page.evaluate(() => document.documentElement.outerHTML)
+    res.writeHead(429)
+    res.end()
   }
-
-  return html
-}
-
-// server should be exported instead of being listened to
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}\n`))
+})
