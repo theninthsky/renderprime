099fb52579877ebdb6383f2e8f15300e500774ad
Almog Gabay <theninesky@gmail.com>
2024-03-31 20:31:02 +0300

    Revert "Allow processing multiple pages concurrently in separate tabs"
    This reverts commit 6bf5b0618db64651b1020fc473f82f088a61bf5e.

--- README.md
+++ README.md
@@ -4,10 +4,8 @@ A fast and serverless prerenderer built as a GCP _[Cloud Function](https://cloud
 
 # Environment Variables
 
-`MAX_PAGES`: The number of open tabs (default: number of cpu cores minus 1).
-
 `USER_AGENT`: The `navigator.userAgent` string that will be injected to the browser during rendering (default: `Prerender`).
 
-`WAIT_AFTER_LAST_REQUEST`: The number of milliseconds to wait after the last request before snapshotting the DOM. This has tight correlation to the CPU power and the `MAX_PAGES` value (default: `200`).
+`WAIT_AFTER_LAST_REQUEST`: The number of milliseconds to wait after the last request before snapshotting the DOM. This has tight correlation to the CPU power (default: `200`).
 
 `WAIT_AFTER_LAST_REQUEST_TIMEOUT`: For how many milliseconds should the browser wait for the last request to return before giving up and snapshotting the DOM anyway (default: `5000`).
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import { availableParallelism } from 'node:os'
 import url from 'node:url'
 import PQueue from 'p-queue'
 import puppeteer from 'puppeteer'
@@ -7,59 +6,26 @@ import functions from '@google-cloud/functions-framework'
 import removeScriptTags from './utils/removeScriptTags.js'
 import removePreloads from './utils/removePreloads.js'
 
-const {
-  MAX_PAGES = availableParallelism() - 1,
-  USER_AGENT = 'Prerender',
-  WAIT_AFTER_LAST_REQUEST = 200,
-  WAIT_AFTER_LAST_REQUEST_TIMEOUT = 5000
-} = process.env
+const { USER_AGENT = 'Prerender', WAIT_AFTER_LAST_REQUEST = 200, WAIT_AFTER_LAST_REQUEST_TIMEOUT = 5000 } = process.env
 const allowlist = ['document', 'script', 'xhr', 'fetch', 'other']
 const blockList = ['.ico']
 
-const queue = new PQueue({ concurrency: MAX_PAGES })
+const queue = new PQueue({ concurrency: 1 })
 const browser = await puppeteer.launch({ args: ['--disable-gpu', '--no-first-run', '--no-sandbox'] })
-const availablePages = []
+const [page] = await browser.pages()
 
-const initializePage = async existingPage => {
-  const page = existingPage || (await browser.newPage())
+await page.setUserAgent(USER_AGENT)
+await page.setViewport({ width: 1440, height: 768 })
+await page.setRequestInterception(true)
 
-  await page.setUserAgent(USER_AGENT)
-  await page.setViewport({ width: 1440, height: 768 })
-  await page.setRequestInterception(true)
-
-  page.on('request', request => {
-    if (request.isInterceptResolutionHandled()) return
-    if (!allowlist.includes(request.resourceType()) || blockList.some(resource => request.url().endsWith(resource))) {
-      return request.abort()
-    }
-
-    request.continue()
-  })
-
-  return page
-}
-
-const renderPage = async websiteUrl => {
-  const page = availablePages.pop() || (await initializePage())
-
-  try {
-    await page.goto(websiteUrl)
-    await page.waitForNetworkIdle({ idleTime: +WAIT_AFTER_LAST_REQUEST, timeout: +WAIT_AFTER_LAST_REQUEST_TIMEOUT })
-  } catch (err) {
-    console.error(err.message)
+page.on('request', request => {
+  if (request.isInterceptResolutionHandled()) return
+  if (!allowlist.includes(request.resourceType()) || blockList.some(resource => request.url().endsWith(resource))) {
+    return request.abort()
   }
 
-  let html = await page.content()
-
-  availablePages.push(page)
-
-  html = removeScriptTags(html)
-  html = removePreloads(html)
-
-  return html
-}
-
-availablePages.push(await initializePage((await browser.pages())[0]))
+  request.continue()
+})
 
 functions.http('render', async (req, res) => {
   const { query } = url.parse(req.url, true)
@@ -84,3 +50,18 @@ functions.http('render', async (req, res) => {
     res.end()
   }
 })
+
+const renderPage = async websiteUrl => {
+  try {
+    await page.goto(websiteUrl)
+    await page.waitForNetworkIdle({ idleTime: +WAIT_AFTER_LAST_REQUEST, timeout: +WAIT_AFTER_LAST_REQUEST_TIMEOUT })
+  } catch (err) {
+    console.error(err.message)
+  }
+
+  let html = await page.content()
+  html = removeScriptTags(html)
+  html = removePreloads(html)
+
+  return html
+}
